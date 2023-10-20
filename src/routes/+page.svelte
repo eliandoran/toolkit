1bc0eaec040f2764e64962a349d43c57f4815b38
Elian Doran <contact@eliandoran.me>
2023-10-20 23:58:54 +0300

    Improve layout slightly

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,43 @@
+<style>
+    main {
+        font-family: sans-serif;
+        max-width: 1024px;
+        margin: auto;
+    }
+
+    h2 {
+        font-size: 13pt;
+        font-weight: 100;
+    }
+
+    textarea {
+        width: 100%;
+        border: 1px solid gray;
+        border-radius: 6px;
+        min-height: 80vh;
+    }
+
+    .input,
+    .output {
+        float: left;
+        width: 50%;
+        padding: 1em;
+        box-sizing: border-box;
+    }
+</style>
+
+<main>
+    <section class="input">
+        <h2>Input</h2>
+        <textarea bind:value={inputText}></textarea>
+    </section>
+
+    <section class="output">
+        <h2>Output</h2>
+        <textarea readonly>{outputText}</textarea>
+    </section>
+</main>
+
 <script>
 	import Base64Tool from "$lib/tools/base64";
 
@@ -9,11 +49,3 @@
         outputText = tool.to(inputText);
     }
 </script>
-
-<section class="input">
-    <textarea bind:value={inputText}></textarea>
-</section>
-
-<section class="output">
-    <textarea readonly>{outputText}</textarea>
-</section>
