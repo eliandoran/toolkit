27bc5c9dfbc3c8b03caa70ddf06cdca1c1425c5d
Elian Doran <contact@eliandoran.me>
2023-10-22 00:14:12 +0300

    Implement collapsible stacks

--- src/lib/components/stack-view.svelte
+++ src/lib/components/stack-view.svelte
@@ -2,19 +2,38 @@
     export let title;
     export let height = "100%";
     export let hasPadding = true;
+
+    let isExpanded = true;    
+
+    function toggleCollapse() {
+        isExpanded = !isExpanded;
+    }
 </script>
 
 <header class="app-header">
     <slot name="header-left" />
     <h2>{title}</h2>
     <slot name="header-right" />
+    <a href="#" on:click={toggleCollapse}>{ isExpanded ? "Collapse" : "Expand" }</a>
 </header>
 
-<div class="inner-wrapper" class:padding={hasPadding} style:height={height}>
+<div class="inner-wrapper"
+    class:padding={hasPadding}
+    class:collapsed={!isExpanded}
+    style:height={height}>
     <slot />
 </div>
 
 <style>
+    .inner-wrapper {
+        max-height: none;
+    }
+
+    .inner-wrapper.collapsed {
+        max-height: 0;
+        overflow: hidden;
+    }
+
     .inner-wrapper.padding {
         padding: 1em;
     }
