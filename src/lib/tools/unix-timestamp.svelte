1fce97c7d75a120b27db87909fbf01142e74afed
Elian Doran <contact@eliandoran.me>
2023-10-21 10:57:21 +0300

    Implement basic timestamp tool

--- src/lib/tools/unix-timestamp.svelte
+++ src/lib/tools/unix-timestamp.svelte
@@ -1,1 +1,23 @@
-Timestamp tool goes here.
+<script>
+	import TwoColumnView from "$lib/components/two-column-view.svelte";
+
+    let inputDate = new Date().toISOString();
+    let outputTimestampMillis;
+
+    $: {
+        const date = new Date(inputDate);
+        outputTimestampMillis = date.getTime();
+    }
+</script>
+
+<TwoColumnView
+    leftTitle="Input date"
+    rightTitle="Unix timestamp">
+    <div slot="left">
+        <input type="datetime-local" bind:value={inputDate} />
+    </div>
+
+    <div slot="right">
+        <input type="number" bind:value={outputTimestampMillis} />
+    </div>
+</TwoColumnView>
