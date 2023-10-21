f8d131a5fc5bf3af5d673f7432f2a7ee29deb794
Elian Doran <contact@eliandoran.me>
2023-10-21 11:13:21 +0300

    unix-timestamp: Separate fields for hours, minutes & seconds

--- src/lib/tools/unix-timestamp.svelte
+++ src/lib/tools/unix-timestamp.svelte
@@ -1,12 +1,29 @@
 <script>
 	import TwoColumnView from "$lib/components/two-column-view.svelte";
 
-    let inputDate = new Date().toISOString();
+    const defaultValue = new Date().toISOString().split("T");
+    let inputDate = defaultValue[0];
     let outputTimestampMillis;
+    
+    let inputTime = defaultValue[1].split(":");
+    let hours = inputTime[0];
+    let minutes = inputTime[1];
+    let seconds = inputTime[2].split(".")[0];
 
+    console.log(inputTime);
     $: {
-        const date = new Date(inputDate);
+        const dateString = `${inputDate}T${pad2(hours)}:${pad2(minutes)}:${pad2(seconds)}Z`;
+        const date = new Date(dateString);
+        console.log(dateString, date);
         outputTimestampMillis = date.getTime();
+    }
+
+    function pad2(value) {
+        if (value < 10) {
+            return "0" + value;
+        } else {
+            return value;
+        }
     }
 </script>
 
@@ -14,7 +31,21 @@
     leftTitle="Input date"
     rightTitle="Unix timestamp">
     <div slot="left">
-        <input type="datetime-local" bind:value={inputDate} />
+        <div>
+            <label>Date</label>
+            <input type="date" bind:value={inputDate} />
+        </div>
+
+        <div>
+            <label>Hours</label>
+            <input type="number" bind:value={hours} />
+
+            <label>Minute</label>
+            <input type="number" bind:value={minutes} />
+
+            <label>Seconds</label>
+            <input type="number" bind:value={seconds} />
+        </div>
     </div>
 
     <div slot="right">
