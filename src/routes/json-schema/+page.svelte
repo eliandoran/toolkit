bc4ea8bdb8754ec51af12b5b1723266f1b5a704b
Elian Doran <contact@eliandoran.me>
2023-10-21 23:36:56 +0300

    json-schema: Split table into different component

--- src/routes/json-schema/+page.svelte
+++ src/routes/json-schema/+page.svelte
@@ -3,6 +3,7 @@
     import Tool from "$lib/components/tool.svelte";
 	import WarningBox from "$lib/components/warning-box.svelte";
     import Ajv from "ajv";
+	import ValidationTable from "./validation-table.svelte";
 
     let inputJsonText;
     let schemaText;
@@ -79,28 +80,7 @@
                 <p>❌ The JSON is not valid against the schema.</p>
             {/if}
     
-            {#if validationErrors && validationErrors.length > 0}
-                <table class="styled-table full-width">
-                    <thead>
-                        <tr>
-                            <th>Keyword</th>
-                            <th>Message</th>
-                            <th>Instance path</th>
-                            <th>Schema path</th>
-                        </tr>                
-                    </thead>
-                    <tbody>
-                        {#each validationErrors as error}
-                        <tr>
-                            <td>{error.keyword}</td>
-                            <td>{error.message}</td>
-                            <td>{error.instancePath}</td>
-                            <td>{error.schemaPath}</td>
-                        </tr>
-                        {/each}
-                    </tbody>
-                </table>
-            {/if}
+            <ValidationTable {validationErrors} />
         </div>
     </StackView>
 
---
+++ src/routes/json-schema/validation-table.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let validationErrors;
+</script>
+
+{#if validationErrors && validationErrors.length > 0}
+    <table class="styled-table full-width">
+        <thead>
+            <tr>
+                <th>Keyword</th>
+                <th>Message</th>
+                <th>Instance path</th>
+                <th>Schema path</th>
+            </tr>                
+        </thead>
+        <tbody>
+            {#each validationErrors as error}
+            <tr>
+                <td>{error.keyword}</td>
+                <td>{error.message}</td>
+                <td>{error.instancePath}</td>
+                <td>{error.schemaPath}</td>
+            </tr>
+            {/each}
+        </tbody>
+    </table>
+{/if}
