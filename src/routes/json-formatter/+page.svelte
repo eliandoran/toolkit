e52ddf3983c78d3380636edede002988ed4491a1
Elian Doran <contact@eliandoran.me>
2023-10-21 21:01:16 +0300

    json-formatter: Use single box

--- src/routes/json-formatter/+page.svelte
+++ src/routes/json-formatter/+page.svelte
@@ -1,20 +1,90 @@
 <script>
-	import SymmetricInputOutput from "$lib/components/symmetric-input-output.svelte";
 	import Tool from "$lib/components/tool.svelte";
 
-    function to(input) {
-        return JSON.stringify(JSON.parse(input), null, 4);
+    let text = "";
+
+    function format() {
+        text = JSON.stringify(JSON.parse(text), null, 4);
     }
 
-    function from(input) {
-        return JSON.stringify(JSON.parse(input));
+    function minify() {
+        text = JSON.stringify(JSON.parse(text));
     }
 </script>
 
 <Tool title="JSON Formatter/Minifier">
-    <SymmetricInputOutput
-        fromTitle="Unformatted/Minified JSON"
-        toTitle="Formatted JSON"
-        {to} {from}
-    />
+    <div class="main-container">
+
+        <main>
+            <textarea bind:value={text}></textarea> 
+        </main>
+
+        <aside>
+            <h3>Format/indent</h3>
+            <section>
+                <nav class="nav">
+                    <ul>
+                        <li>
+                            <a href="#" on:click={format}>Prettify</a>
+                        </li>
+
+                        <li>
+                            <a href="#" on:click={minify}>Minify</a>
+                        </li>
+                    </ul>
+                </nav>
+            </section>
+        </aside>
+
+    </div>
 </Tool>
+
+<!-- TODO: Deduplicate (see text-analyer) -->
+<style>
+    textarea {
+        width: 100%;
+        min-height: 70vh;
+    }
+
+    .main-container {
+        display: flex;
+    }
+
+    .main-container > main {
+        flex-grow: 1;
+    }
+
+    .main-container > aside {
+        padding-left: 1em;
+        min-width: 200px;
+    }
+
+    .main-container > aside nav {
+        background: var(--content-background-color);
+        padding: 0.5em;
+        border-radius: 12px;
+    }
+
+    .main-container > aside nav ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+    }
+
+    .main-container > aside nav a {
+        text-decoration: none;
+        color: inherit;
+        font-size: 0.9em;
+    }
+
+    aside h3 {
+        font-size: 0.75em;
+        font-weight: 400;        
+    }
+
+    aside section {
+        background: var(--content-background-color);
+        padding: 0.5em;
+        border-radius: 12px;
+    }
+</style>
