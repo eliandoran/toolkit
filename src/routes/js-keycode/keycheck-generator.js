e710acbac3c5cdab1af4ec03b4dfa11577125765
Elian Doran <contact@eliandoran.me>
2023-10-23 00:21:29 +0300

    js-keycode: Support more meta combinations

--- src/routes/js-keycode/keycheck-generator.js
+++ src/routes/js-keycode/keycheck-generator.js
@@ -6,16 +6,26 @@ export default function generateKeyCheckExpression(e) {
     if (!isMeta) {
         lines.push(`const isMeta = (e.ctrlKey || e.altKey || e.metaKey);`)
         lines.push(`const isPressed = (key == "${e.key}")`);  
-    } else if (e.ctrlKey && !e.altKey && !e.metaKey) {
-        lines.push(`const isCtrl = (e.ctrlKey && !e.altKey && !e.metaKey)`);
-        lines.push(`const isPressed = (isCtrl && key == "${e.key}")`);  
-    } else if (!e.ctrlKey && e.altKey && !e.metaKey) {
-        lines.push(`const isAlt = (!e.ctrlKey && e.altKey && !e.metaKey)`);
-        lines.push(`const isPressed = (isAlt && key == "${e.key}")`);  
-    } else if (!e.ctrlKey && !e.altKey && e.metaKey) {
-        lines.push(`const isMeta = (!e.ctrlKey && !e.altKey && e.metaKey)`);
-        lines.push(`const isPressed = (isMeta && key == "${e.key}")`);  
+    } else {
+        lines.push(`const isRightMeta = (${getMetaKeyCheckExpression(e)})`);
+        lines.push(`const isPressed = (isRightMeta && key == "${e.key}")`);  
     }
-
+    
     return lines.join("\n");
+}
+
+function getMetaKeyCheckExpression(e) {
+    return [
+        negateIfFalse("e.ctrlKey", e.ctrlKey),
+        negateIfFalse("e.altKey", e.altKey),
+        negateIfFalse("e.metaKey", e.metaKey)
+    ].join(" && ");
+}
+
+function negateIfFalse(expr, boolean) {
+    if (boolean) {
+        return expr;
+    } else {
+        return "!" + expr;
+    }
 }
