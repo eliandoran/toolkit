c167e56ab3c918f81a17055cebe59ee84c37ccbb
Elian Doran <contact@eliandoran.me>
2023-10-22 13:52:20 +0300

    Add icons to tools menu

--- src/lib/tools.js
+++ src/lib/tools.js
@@ -1,47 +1,57 @@
+import { BxCalculator, BxColor, BxFile, BxHash, BxLink, BxListCheck, BxTimer, BxsFileExport, BxsFileJson } from "svelte-boxicons";
+
 export default {
     "Plain text": [
         {
             path: "/plain-text",
-            title: "Text tools"
+            title: "Text tools",
+            icon: BxFile
         },
     
         {
             path: "/text-hash",
-            title: "Text hash"
+            title: "Text hash",
+            icon: BxHash
         },
     
         {
             path: "/ansi-text",
-            title: "ANSI text viewer"
+            title: "ANSI text viewer",
+            icon: BxColor
         }
     ],
 
     Arithmetics: [
         {
             path: "/percentage-calculator",
-            title: "Percentage calculator"
+            title: "Percentage calculator",
+            icon: BxCalculator
         }
     ],
 
     "Date & Time": [
         {
             path: "/unix-timestamp",
-            title: "Unix timestamp"
+            title: "Unix timestamp",
+            icon: BxTimer
         }
     ],
 
     "JSON": [
         {
             path: "/json-formatter",
-            title: "JSON formatter"
+            title: "JSON formatter",
+            icon: BxsFileJson
         },
         {
             path: "/json-converter",
-            title: "JSON to YAML"
+            title: "JSON to YAML",
+            icon: BxsFileExport
         },
         {
             path: "/json-schema",
-            title: "JSON Schema Validator"
+            title: "JSON Schema Validator",
+            icon: BxListCheck
         }
     ],
 
@@ -52,7 +62,8 @@ export default {
         },
         {
             path: "/url-encoder",
-            title: "URL Encode"
+            title: "URL Encode",
+            icon: BxLink
         },
         {
             path: "/html-entity",
--- src/routes/tools-menu.svelte
+++ src/routes/tools-menu.svelte
@@ -1,6 +1,7 @@
 <script>
     import { page } from "$app/stores";
     import tools from "$lib/tools.js";
+	import Icon from "../lib/components/icon.svelte";
 </script>
 
 {#each Object.entries(tools) as [categoryName, categoryItems]}
@@ -10,9 +11,16 @@
         <nav class="nav">
             <ul>
                 {#each categoryItems as tool}
-                    <li>
+                    <li>                        
                         <a href="{tool.path}"
-                            class:active="{tool.path === $page.url.pathname}">{tool.title}</a>
+                            class:active="{tool.path === $page.url.pathname}">
+                            <span class="icon">
+                                {#if tool.icon}
+                                    <Icon icon={tool.icon} />
+                                {/if}
+                            </span>
+                            {tool.title}
+                        </a>
                     </li>
                 {/each}
             </ul>
@@ -34,6 +42,20 @@
 
     section:first-of-type {
         margin-top: 0;
+    }
+
+    .icon {
+        padding-left: 2em;
+        position: relative;
+    }
+
+    .icon :global(svg) {
+        position: absolute;
+        left: 0;
+        top: 50%;        
+        width: 21px;
+        height: 21px;
+        transform: translateY(-50%);
     }
 
 </style>
