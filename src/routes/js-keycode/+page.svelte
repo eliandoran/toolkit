5e232f89ef779432f3a52ae4fb33355a10ddadc0
Elian Doran <contact@eliandoran.me>
2023-10-22 23:29:51 +0300

    js-keycode: Improve style slightly

--- src/routes/js-keycode/+page.svelte
+++ src/routes/js-keycode/+page.svelte
@@ -22,16 +22,42 @@
 
 <Tool>
     <OneColumnView title="JavaScript Keycode">        
-        <Card title="key">
-            <span class="big-value">{key}</span>
-        </Card>
+        <div class="columns">
+            <div class="column">
+                <Card title="key">
+                    <span class="big-value">{key}</span>
+                </Card>
+            </div>
 
-        <Card title="charCode">
-            <span class="big-value">{charCode}</span>
-        </Card>
-
-        <Card title="keyCode">
-            <span class="big-value">{keyCode}</span>
-        </Card>
+            <div class="column">
+                <Card title="charCode">
+                    <span class="big-value">{charCode}</span>
+                </Card>
+            </div>
+    
+            <div class="column">
+                <Card title="keyCode">
+                    <span class="big-value">{keyCode}</span>
+                </Card>
+            </div>
+        </div>
     </OneColumnView>
 </Tool>
+
+<style>
+    .columns {
+        display: flex;
+        column-gap: 1em;
+    }
+
+    .column {
+        flex-grow: 1;        
+    }
+
+    .big-value {
+        display: block;
+        text-align: center;
+        font-size: 3em;
+        font-weight: 100;
+    }
+</style>
