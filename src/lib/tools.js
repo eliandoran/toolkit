4689c8d35694bdc0e01247bff25faafd6ca22d17
Elian Doran <contact@eliandoran.me>
2023-10-22 12:51:02 +0300

    Add categories to menu

--- src/lib/tools.js
+++ src/lib/tools.js
@@ -1,56 +1,62 @@
-export default [
-    {
-        path: "/percentage-calculator",
-        title: "Percentage calculator"
-    },
+export default {
+    "Plain text": [
+        {
+            path: "/plain-text",
+            title: "Text tools"
+        },
+    
+        {
+            path: "/text-hash",
+            title: "Text hash"
+        },
+    
+        {
+            path: "/ansi-text",
+            title: "ANSI text"
+        }
+    ],
 
-    {
-        path: "/unix-timestamp",
-        title: "Unix timestamp"
-    },
+    Arithmetics: [
+        {
+            path: "/percentage-calculator",
+            title: "Percentage calculator"
+        }
+    ],
 
-    {
-        path: "/json-formatter",
-        title: "JSON Formatter/Minifier"
-    },
+    "Date & Time": [
+        {
+            path: "/unix-timestamp",
+            title: "Unix timestamp"
+        }
+    ],
 
-    {
-        path: "/json-converter",
-        title: "JSON<->YAML converter"
-    },
+    "JSON": [
+        {
+            path: "/json-formatter",
+            title: "JSON Formatter/Minifier"
+        },
+        {
+            path: "/json-converter",
+            title: "JSON<->YAML converter"
+        },
+        {
+            path: "/json-schema",
+            title: "JSON Schema Validator"
+        }
+    ],
 
-    {
-        path: "/base64",
-        title: "Base64 Encoder/Decoder"
-    },
-
-    {
-        path: "/url-encoder",
-        title: "URL Encoder/Decoder"
-    },
-
-    {
-        path: "/html-entity",
-        title: "HTML Entity Encode/Decode"
-    },
-
-    {
-        path: "/plain-text",
-        title: "Text tools"
-    },
-
-    {
-        path: "/text-hash",
-        title: "Text hash"
-    },
-
-    {
-        path: "/ansi-text",
-        title: "ANSI text"
-    },
-
-    {
-        path: "/json-schema",
-        title: "JSON Schema Validator"
-    }
-];
+    "Encode/Decode": [
+        {
+            path: "/base64",
+            title: "Base64 Encoder/Decoder"
+        },
+        {
+            path: "/url-encoder",
+            title: "URL Encoder/Decoder"
+        },
+        {
+            path: "/html-entity",
+            title: "HTML Entity Encode/Decode"
+        }
+    ]    
+};
--- src/routes/tools-menu.svelte
+++ src/routes/tools-menu.svelte
@@ -3,13 +3,37 @@
     import tools from "$lib/tools.js";
 </script>
 
-<nav class="nav">
-    <ul>
-        {#each tools as tool}
-        <li>
-            <a href="{tool.path}"
-                class:active="{tool.path === $page.url.pathname}">{tool.title}</a>
-        </li>
-        {/each}
-    </ul>
-</nav>
+{#each Object.entries(tools) as [categoryName, categoryItems]}
+    <section>
+        <h3>{categoryName}</h3>
+    
+        <nav class="nav">
+            <ul>
+                {#each categoryItems as tool}
+                    <li>
+                        <a href="{tool.path}"
+                            class:active="{tool.path === $page.url.pathname}">{tool.title}</a>
+                    </li>
+                {/each}
+            </ul>
+        </nav>
+    </section>
+{/each}
+
+<style>
+
+    h3 {
+        margin: 0;
+        font-size: 0.9em;
+        opacity: 0.8;
+    }
+
+    section {
+        margin: 1em 0;
+    }
+
+    section:first-of-type {
+        margin-top: 0;
+    }
+
+</style>
