524c9d979a102103af636e5c0c5c7025417c384d
Elian Doran <contact@eliandoran.me>
2023-10-23 00:13:41 +0300

    js-keycode: Add basic checking for single meta key

--- src/routes/js-keycode/+page.svelte
+++ src/routes/js-keycode/+page.svelte
@@ -19,10 +19,19 @@
         const lines = [];
         const isMeta = (e.ctrlKey || e.altKey || e.metaKey)
 
-        // Normal key, check that meta keys are not pressed.
+        // Normal key, check that meta keys are not pressed.        
         if (!isMeta) {
             lines.push(`const isMeta = (e.ctrlKey || e.altKey || e.metaKey);`)
             lines.push(`const isPressed = (key == "${e.key}")`);  
+        } else if (e.ctrlKey && !e.altKey && !e.metaKey) {
+            lines.push(`const isCtrl = (e.ctrlKey && !e.altKey && !e.metaKey)`);
+            lines.push(`const isPressed = (isCtrl && key == "${e.key}")`);  
+        } else if (!e.ctrlKey && e.altKey && !e.metaKey) {
+            lines.push(`const isAlt = (!e.ctrlKey && e.altKey && !e.metaKey)`);
+            lines.push(`const isPressed = (isAlt && key == "${e.key}")`);  
+        } else if (!e.ctrlKey && !e.altKey && e.metaKey) {
+            lines.push(`const isMeta = (!e.ctrlKey && !e.altKey && e.metaKey)`);
+            lines.push(`const isPressed = (isMeta && key == "${e.key}")`);  
         }
 
         return lines.join("\n");
