ef9db744773ff176504754e582dfc16f69f77aa6
Elian Doran <contact@eliandoran.me>
2023-10-22 12:26:11 +0300

    codemirror: Use light theme based on user preference

--- src/lib/stores/codemirror.js
+++ src/lib/stores/codemirror.js
@@ -1,5 +1,19 @@
 import { oneDark } from "@codemirror/theme-one-dark";
 import { readable } from "svelte/store";
+import { browser } from "$app/environment";
 
-const defaultTheme = oneDark;
+function getDefaultTheme() {
+    if (!browser) {
+        return;
+    }
+
+    const prefersDark = (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
+    if (prefersDark) {
+        return oneDark;
+    }
+    
+    return undefined;
+}
+
+const defaultTheme = getDefaultTheme();
 export const theme = readable(defaultTheme);
