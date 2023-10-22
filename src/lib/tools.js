9bf6454318da020b40458fc02ae54da641db1453
Elian Doran <contact@eliandoran.me>
2023-10-22 12:40:26 +0300

    Move tools list to dedicated file

---
+++ src/lib/tools.js
@@ -0,0 +1,56 @@
+export default [
+    {
+        path: "/percentage-calculator",
+        title: "Percentage calculator"
+    },
+
+    {
+        path: "/unix-timestamp",
+        title: "Unix timestamp"
+    },
+
+    {
+        path: "/json-formatter",
+        title: "JSON Formatter/Minifier"
+    },
+
+    {
+        path: "/json-converter",
+        title: "JSON<->YAML converter"
+    },
+
+    {
+        path: "/base64",
+        title: "Base64 Encoder/Decoder"
+    },
+
+    {
+        path: "/url-encoder",
+        title: "URL Encoder/Decoder"
+    },
+
+    {
+        path: "/html-entity",
+        title: "HTML Entity Encode/Decode"
+    },
+
+    {
+        path: "/plain-text",
+        title: "Text tools"
+    },
+
+    {
+        path: "/text-hash",
+        title: "Text hash"
+    },
+
+    {
+        path: "/ansi-text",
+        title: "ANSI text"
+    },
+
+    {
+        path: "/json-schema",
+        title: "JSON Schema Validator"
+    }
+];
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,62 +1,6 @@
 <script>
 	import { page } from "$app/stores";
-
-    const tools = [
-        {
-            path: "/percentage-calculator",
-            title: "Percentage calculator"
-        },
-
-        {
-            path: "/unix-timestamp",
-            title: "Unix timestamp"
-        },
-
-        {
-            path: "/json-formatter",
-            title: "JSON Formatter/Minifier"
-        },
-
-        {
-            path: "/json-converter",
-            title: "JSON<->YAML converter"
-        },
-
-        {
-            path: "/base64",
-            title: "Base64 Encoder/Decoder"
-        },
-
-        {
-            path: "/url-encoder",
-            title: "URL Encoder/Decoder"
-        },
-
-        {
-            path: "/html-entity",
-            title: "HTML Entity Encode/Decode"
-        },
-
-        {
-            path: "/plain-text",
-            title: "Text tools"
-        },
-
-        {
-            path: "/text-hash",
-            title: "Text hash"
-        },
-
-        {
-            path: "/ansi-text",
-            title: "ANSI text"
-        },
-
-        {
-            path: "/json-schema",
-            title: "JSON Schema Validator"
-        }
-    ];
+    import tools from "$lib/tools.js";
 </script>
 
 <div class="page-wrapper">
