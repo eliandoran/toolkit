fd7294878c19a97e17c88f375e52d76d71d7916c
Elian Doran <contact@eliandoran.me>
2023-10-22 11:36:00 +0300

    percentage-calculator: Move titles outside of card

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -2,29 +2,29 @@
     export let title = undefined;
 </script>
 
+{#if title}
+<h3>{title}</h3>
+{/if}
+
 <section>
-
-    {#if title}
-    <h3>{title}</h3>
-    {/if}
-
     <slot />
-
 </section>
 
 <style>
-    section {
-        background: var(--content-background-color);
-        padding: 1.5em;
-        border-radius: 12px;
-        margin: 1em 0;
-        border: 1px solid var(--border-color);
-    }
-
-    section h3 {
+    h3 {
         margin: 0;
         margin-bottom: 0.5em;
         font-size: 0.8em;
         font-weight: 800;
+        opacity: 0.8;
     }
+
+    section {
+        background: var(--content-background-color);
+        padding: 1.5em;
+        border-radius: 12px;
+        margin: 1.5em 0;
+        margin-top: 0.25em;        
+        border: 1px solid var(--border-color);
+    }    
 </style>
