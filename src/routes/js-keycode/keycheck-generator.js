c4bfb4d4d76e29a6815ff90405a72bda35db9d0f
Elian Doran <contact@eliandoran.me>
2023-10-23 00:36:39 +0300

    js-keycode: Add single command key support

--- src/routes/js-keycode/keycheck-generator.js
+++ src/routes/js-keycode/keycheck-generator.js
@@ -1,9 +1,20 @@
+const allCommandKeys = [
+    "CapsLock",
+    "ControlLeft", "ControlRight",
+    "AltLeft", "AltRight",
+    "ShiftLeft", "ShiftRight",
+];
+
 export default function generateKeyCheckExpression(e) {
     const lines = [];
-    const isMeta = (e.ctrlKey || e.altKey || e.metaKey)
+    const isMeta = (e.ctrlKey || e.altKey || e.metaKey);
+    const isSingleCommandKey = (allCommandKeys.includes(e.code));
 
-    // Normal key, check that meta keys are not pressed.        
-    if (!isMeta) {
+    if (isSingleCommandKey) {
+        // Single command key is pressed (e.g. CtrlLeft).
+        lines.push(`const isPressed = (e.code == "${e.code}")`);
+    } else if (!isMeta) {
+        // Normal key, check that meta keys are not pressed.            
         lines.push(`const isMeta = (e.ctrlKey || e.altKey || e.metaKey);`)
         lines.push(`const isPressed = (!isMeta && key == "${e.key}")`);  
     } else {
