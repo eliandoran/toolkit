16648f8dcd99286c1d8cab4a4302296fba7b9b9e
Elian Doran <contact@eliandoran.me>
2024-07-06 00:37:43 +0300

    jwt-decoder: Display validation errors

--- src/routes/jwt-decoder/+page.svelte
+++ src/routes/jwt-decoder/+page.svelte
@@ -3,38 +3,44 @@
 
 	import Tool from "$lib/components/tool.svelte";
 	import TwoColumnView from "$lib/components/two-column-view.svelte";
+	import WarningBox from "$lib/components/warning-box.svelte";
 
     let encodedJwt;
     let decodedJwt;
     let formattedDecodedJwt = "";
-
-    function onInput() {
-        console.log(encodedJwt);
-    }
+    let error;
 
     $: {
+        error = "";
+        formattedDecodedJwt = "";
+
         try {
             if (encodedJwt) {
                 decodedJwt = jwtDecode(encodedJwt);
                 formattedDecodedJwt = JSON.stringify(decodedJwt, null, 4);
             }
         } catch (e) {
-            if (!(e instanceof InvalidTokenError)) {
+            if ((e instanceof InvalidTokenError)) {
+                error = e.message || "Unknown error while decoding the JWT token.";
+            } else {
                 throw e;
             }
         }
-        console.log(decodedJwt);
     }
 </script>
 
 <Tool>
-    <TwoColumnView leftTitle="Input the JSON Web Token (JWT)" rightTitle="Info">
+    <TwoColumnView leftTitle="Input the JSON Web Token (JWT)" rightTitle="Decoded JWT">
         <div slot="left">
             <textarea bind:value={encodedJwt}></textarea>
         </div>
 
         <div slot="right">
-            <textarea value={formattedDecodedJwt} readonly></textarea>
+            <WarningBox message={error} />
+
+            {#if formattedDecodedJwt}
+                <textarea value={formattedDecodedJwt} readonly></textarea>
+            {/if}
         </div>
     </TwoColumnView>
 </Tool>
