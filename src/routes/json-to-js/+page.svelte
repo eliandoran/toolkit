5e65533260715ff7111238556534945a674a5534
Elian Doran <contact@eliandoran.me>
2024-07-05 20:35:30 +0300

    json-to-js: Add indentation

--- src/routes/json-to-js/+page.svelte
+++ src/routes/json-to-js/+page.svelte
@@ -4,23 +4,42 @@
 
   function to(input) {
     const data = JSON.parse(input);
-    return convertObjectToJsNotation(data);
+    return convertObjectToJsNotation(data, 0);
   }
 
-  function convertObjectToJsNotation(obj) {
-    if (typeof obj === 'object' && !Array.isArray(obj)) {
-      let entries = Object.entries(obj).map(([key, value]) => {
-        return `${key}: ${convertObjectToJsNotation(value)}`;
-      });
-      return `{ ${entries.join(', ')} }`;
-    } else if (Array.isArray(obj)) {
-      let values = obj.map(value => convertObjectToJsNotation(value));
-      return `[ ${values.join(', ')} ]`;
-    } else if (typeof obj === 'string') {
-      return `"${obj}"`;
-    } else {
-      return obj;
+  function convertObjectToJsNotation(obj, indentLevel) {
+    const indentUnit = "  ";
+    let indent = indentUnit.repeat(indentLevel);
+    
+    // Array
+    if (Array.isArray(obj)) {
+      let lines = [];
+      lines.push(`[`);
+      for (const value of obj) {
+        lines.push(`${indent + indentUnit}${convertObjectToJsNotation(value, indentLevel+1)},`);
+      }
+      lines.push(`${indent}]`);
+      return lines.join("\n");
     }
+
+    // Object
+    if (typeof obj === 'object' && !Array.isArray(obj)) {
+      let lines = [];
+      lines.push("{");
+      for (const [ key, value ] of Object.entries(obj)) {
+        lines.push(`${indent + indentUnit}${key}: ${convertObjectToJsNotation(value, indentLevel+1)},`);
+      }
+      lines.push(`${indent}}`);
+      return lines.join("\n");
+    }
+    
+    // Strings
+    if (typeof obj === "string") {
+      return `"${obj}"`;
+    }
+
+    // Everything else
+    return obj;
   }
 
   function from(input) {
