1715175f62f75515fae25b2fd073c06b16eef756
Elian Doran <contact@eliandoran.me>
2024-07-05 21:00:56 +0300

    json-to-js: Use JSON5 for maximum compatibility

--- src/routes/json-to-js/+page.svelte
+++ src/routes/json-to-js/+page.svelte
@@ -1,49 +1,14 @@
 <script>
 	import SymmetricInputOutput from "$lib/components/symmetric-input-output.svelte";
   import Tool from "$lib/components/tool.svelte";
+  import JSON5 from "json5";
 
   function to(input) {
-    const data = JSON.parse(input);
-    return convertObjectToJsNotation(data, 0);
-  }
-
-  function convertObjectToJsNotation(obj, indentLevel) {
-    const indentUnit = "  ";
-    let indent = indentUnit.repeat(indentLevel);
-    
-    // Array
-    if (Array.isArray(obj)) {
-      let lines = [];
-      lines.push(`[`);
-      for (const value of obj) {
-        lines.push(`${indent + indentUnit}${convertObjectToJsNotation(value, indentLevel+1)},`);
-      }
-      lines.push(`${indent}]`);
-      return lines.join("\n");
-    }
-
-    // Object
-    if (typeof obj === 'object' && !Array.isArray(obj)) {
-      let lines = [];
-      lines.push("{");
-      for (const [ key, value ] of Object.entries(obj)) {
-        lines.push(`${indent + indentUnit}${key}: ${convertObjectToJsNotation(value, indentLevel+1)},`);
-      }
-      lines.push(`${indent}}`);
-      return lines.join("\n");
-    }
-    
-    // Strings
-    if (typeof obj === "string") {
-      return `"${obj}"`;
-    }
-
-    // Everything else
-    return obj;
+    return JSON5.stringify(JSON.parse(input), null, 2);
   }
 
   function from(input) {
-
+    return JSON.stringify(JSON5.parse(input), null, 2);
   }
 </script>
 
