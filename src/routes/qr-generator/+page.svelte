22254df38aa0a4c373e2503bc80b078b1e55d54f
Elian Doran <contact@eliandoran.me>
2024-07-07 01:53:26 +0300

    qr-generator: Display QR code

--- src/routes/qr-generator/+page.svelte
+++ src/routes/qr-generator/+page.svelte
@@ -1,11 +1,36 @@
 <script>
+  import QrCode from "@castlenine/svelte-qrcode";
+
 	import Tool from "$lib/components/tool.svelte";
 	import TwoColumnView from "$lib/components/two-column-view.svelte";
 
+  let data = "";
+  let key;
+
+  function rebuildQr(data) {
+    key = {};
+  }
+
+  $: {
+    rebuildQr(data);
+  }
 </script>
 
 <Tool>
   <TwoColumnView leftTitle="Settings" rightTitle="Generated QR">
+    <div slot="left">
+      <label>
+        Data:
+        <textarea bind:value={data} />
+      </label>
+    </div>
 
+    <div slot="right">
+      {#if data}
+        {#key key}
+          <QrCode {data} />
+        {/key}
+      {/if}
+    </div>
   </TwoColumnView>
 </Tool>
