f9f73b0630648df657730b02284a667e32d4d456
Elian Doran <contact@eliandoran.me>
2024-07-06 11:49:43 +0300

    unicode-analyzer: Use monospace

--- src/routes/unicode-analyzer/+page.svelte
+++ src/routes/unicode-analyzer/+page.svelte
@@ -29,5 +29,10 @@
         border: 0;
         width: 100%;
         height: 100%;
+        padding: 1em;
+    }
+
+    .character-view {
+        font-family: var(--font-monospace);
     }
 </style>
