1364c26adb91410c0e574746e7c2ce550ae458a6
Elian Doran <contact@eliandoran.me>
2024-07-06 17:30:44 +0300

    svg-to-data: Add input -> output

--- src/routes/svg-to-data/+page.svelte
+++ src/routes/svg-to-data/+page.svelte
@@ -1,13 +1,13 @@
 <script>
 	import SymmetricInputOutput from "$lib/components/symmetric-input-output.svelte";
   import Tool from "$lib/components/tool.svelte";
+  import toUri from "mini-svg-data-uri";
 
   function to(svg) {
-  
+    return toUri(svg);
   }
 
   function from(dataUri) {
-
   }
 </script>
 
