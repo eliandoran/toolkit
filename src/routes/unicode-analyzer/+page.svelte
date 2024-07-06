b2545a6ee942aa6dd51f0c168a163f42208cca45
Elian Doran <contact@eliandoran.me>
2024-07-06 16:24:18 +0300

    unicode-analyzer: Display HTML entity

--- src/routes/unicode-analyzer/+page.svelte
+++ src/routes/unicode-analyzer/+page.svelte
@@ -2,6 +2,7 @@
 	import StackView from "$lib/components/stack-view.svelte";
     import Tool from "$lib/components/tool.svelte";
     import { get_unicode_by_decimal } from "unicode-information";
+    import he from "he";
 
     let text = `\
 Control characters:
@@ -9,6 +10,7 @@ Control characters:
 \t- Horizontal tab: \t, vertical tab: \v
 \t- Form feed: \f, carriage return: \r, line feed: \n
 Normalization: e\u0301 vs \u00E9
+Dashes: -–—―⁓
 Combining characters: กิิิิิิิิิิิิิิิิิิิิ ก้้้้้้้้้้้้้้้้้้้้ ก็็็็็็็็็็็็็็็็็็็็ ก็็็็็็็็็็็็็็็็็็็็ กิิิิิิิิิิิิิิิิิิิิ ก้้้้้้้้้้้้้้้้้้้้ ก็็็็็็็็็็็็็็็็็็็็ กิิิิิิิิิิิิิิิิิิิิ ก้้้้้้้้้้้้้้้้้้้้ กิิิิิิิิิิิิิิิิิิิิ ก้้้้้้้้้้้้้้้้้้้้ ก็็็็็็็็็็็็็็็็็็็็ ก็็็็็็็็็็็็็็็็็็็็ กิิิิิิิิิิิิิิิิิิิิ ก้้้้้้้้้้้้้้้้้้้้ ก็็็็็็็็็็็็็็็็็็็็ กิิิิิิิิิิิิิิิิิิิิ ก้้้้้้้้้้้้้้้้้้้้
 
 Combining diacritical marks: \u0300\u0301\u0302\u0303\u0304\u0305\u0306\u0307\u0308\u0309\u030a\u030b\u030c\u030d\u030e\u030f\u0310\u0311\u0312\u0313\u0314\u0315\u0316\u0317\u0318\u0319\u031a\u031b\u031c\u031d\u031e\u031f\u0320\u0321\u0322\u0323\u0324\u0325\u0326\u0327\u0328\u0329\u032a\u032b\u032c\u032d\u032e\u032f\u0330\u0331\u0332\u0333\u0334\u0335\u0336\u0337\u0338\u0339\u033a\u033b\u033c\u033d\u033e\u033f\u0340\u0341\u0342\u0343\u0344\u0345\u0346\u0347\u0348\u0349\u034a\u034b\u034c\u034d\u034e\u034f\u0350\u0351\u0352\u0353\u0354\u0355\u0356\u0357\u0358\u0359\u035a\u035b\u035c\u035d\u035e\u035f\u0360\u0361\u0362\u0363\u0364\u0365\u0366\u0367\u0368\u0369\u036a\u036b\u036c\u036d\u036e\u036f
@@ -60,6 +62,9 @@ Combining diacritical marks: \u0300\u0301\u0302\u0303\u0304\u0305\u0306\u0307\u0
         }
 
         const info = get_unicode_by_decimal(ch.charCodeAt(0))
+        info.htmlEntity = he.encode(ch, {
+            useNamedReferences: true
+        });
         dataCodeLookup[code] = info;
                     
         const builtData = buildNewCharacter(ch, info);        
@@ -151,6 +156,11 @@ Combining diacritical marks: \u0300\u0301\u0302\u0303\u0304\u0305\u0306\u0307\u0
                         <div>
                             <span>Code point</span>
                             <span>{currentInfo.cp}</span>
+                        </div>
+
+                        <div>
+                            <span>HTML entity</span>
+                            <span>{currentInfo.htmlEntity}</span>
                         </div>
 
                         <div>
