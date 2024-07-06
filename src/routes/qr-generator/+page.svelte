875e01cdbcd4fcafa9108003841160571aae4312
Elian Doran <contact@eliandoran.me>
2024-07-07 02:08:56 +0300

    qr-generator: Add error correction level

--- src/routes/qr-generator/+page.svelte
+++ src/routes/qr-generator/+page.svelte
@@ -7,13 +7,14 @@
   let data = "https://eliandoran.github.io/toolkit/";
   let key;
   let typeNumber = 40;
+  let errorCorrectionLevel = "M";
 
   function rebuildQr(...args) {
     key = {};
   }
 
   $: {
-    rebuildQr(data, typeNumber);
+    rebuildQr(data, typeNumber, errorCorrectionLevel);
   }
 </script>
 
@@ -29,12 +30,36 @@
         Type:
         <input type="number" min="0" max="40" bind:value={typeNumber} />
       </label>
+
+      <div>
+        Error correction level:
+
+        <label>
+          <input type="radio" bind:group={errorCorrectionLevel} value="L" />
+          L (~7%)
+        </label>
+
+        <label>
+          <input type="radio" bind:group={errorCorrectionLevel} value="M" />
+          M (~15%)
+        </label>
+
+        <label>
+          <input type="radio" bind:group={errorCorrectionLevel} value="Q" />
+          Q (~25%)
+        </label>
+
+        <label>
+          <input type="radio" bind:group={errorCorrectionLevel} value="H" />
+          H (~30%)
+        </label>
+      </div>
     </div>
 
     <div slot="right">
       {#if data}
         {#key key}
-          <QrCode {data} {typeNumber} />
+          <QrCode {data} {typeNumber} {errorCorrectionLevel} />
         {/key}
       {/if}
     </div>
