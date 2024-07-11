2d22b231fa5f6f9f091e8ecb3405bb3a21057b60
Elian Doran <contact@eliandoran.me>
2024-07-11 18:51:28 +0300

    unit-conversion/data: Swap columns with rows

--- src/routes/unit-conversion/data/+page.svelte
+++ src/routes/unit-conversion/data/+page.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <Tool>
-  <TwoColumnView leftTitle="Bi-bytes (common)" rightTitle="International System of Units">
+  <TwoColumnView leftTitle="Bi-bytes (common)" rightTitle="Data rates (bi-bits, common)">
     <div slot="left">
       <SingleUnit bind:baseValue={baseValue} bind:from={from} to="bytes" name="Bytes" />
       <SingleUnit bind:baseValue={baseValue} bind:from={from} to="kibibytes" name="Kibibytes" />
@@ -19,23 +19,23 @@
     </div>
 
     <div slot="right">
+      <SingleUnit bind:baseValue={baseValue} bind:from={from} to="bits" name="Bits" />
+      <SingleUnit bind:baseValue={baseValue} bind:from={from} to="kibibits" name="Kibibits" />
+      <SingleUnit bind:baseValue={baseValue} bind:from={from} to="mebibits" name="Mebibits" />
+      <SingleUnit bind:baseValue={baseValue} bind:from={from} to="gibibits" name="Gibibits" />
+      <SingleUnit bind:baseValue={baseValue} bind:from={from} to="tebibits" name="Tebibits" />
+      <SingleUnit bind:baseValue={baseValue} bind:from={from} to="pebibits" name="Pebibits" />      
+    </div>
+  </TwoColumnView>
+
+  <TwoColumnView leftTitle="International System of Units" rightTitle="Data rates (bits, International System of Units)">
+    <div slot="left">
       <SingleUnit bind:baseValue={baseValue} bind:from={from} to="bytes" name="Bytes" />
       <SingleUnit bind:baseValue={baseValue} bind:from={from} to="kilobytes" name="Kilobytes" />
       <SingleUnit bind:baseValue={baseValue} bind:from={from} to="megabytes" name="Megabytes" />
       <SingleUnit bind:baseValue={baseValue} bind:from={from} to="gigabytes" name="Gigabytes" />
       <SingleUnit bind:baseValue={baseValue} bind:from={from} to="terabytes" name="Terabytes" />
       <SingleUnit bind:baseValue={baseValue} bind:from={from} to="petabytes" name="Petabytes" />
-    </div>
-  </TwoColumnView>
-
-  <TwoColumnView leftTitle="Data rates (bi-bits, common)" rightTitle="Data rates (bits, International System of Units)">
-    <div slot="left">
-      <SingleUnit bind:baseValue={baseValue} bind:from={from} to="bits" name="Bits" />
-      <SingleUnit bind:baseValue={baseValue} bind:from={from} to="kibibits" name="Kibibits" />
-      <SingleUnit bind:baseValue={baseValue} bind:from={from} to="mebibits" name="Mebibits" />
-      <SingleUnit bind:baseValue={baseValue} bind:from={from} to="gibibits" name="Gibibits" />
-      <SingleUnit bind:baseValue={baseValue} bind:from={from} to="tebibits" name="Tebibits" />
-      <SingleUnit bind:baseValue={baseValue} bind:from={from} to="pebibits" name="Pebibits" />
     </div>
 
     <div slot="right">
