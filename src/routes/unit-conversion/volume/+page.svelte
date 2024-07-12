4cd5d206456fcd867c1e163215a4828735e8d8d3
Elian Doran <contact@eliandoran.me>
2024-07-12 21:53:05 +0300

    unit-conversion/volume: Add fluid ounces

--- src/routes/unit-conversion/volume/+page.svelte
+++ src/routes/unit-conversion/volume/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	import Card from "$lib/components/card.svelte";
+	import StackView from "$lib/components/stack-view.svelte";
 	import Tool from "$lib/components/tool.svelte";
 	import TwoColumnView from "$lib/components/two-column-view.svelte";
 	import SingleUnit from "../single-unit.svelte";
@@ -8,7 +10,7 @@
 </script>
 
 <Tool>    
-    <TwoColumnView leftTitle="Metric units" rightTitle="Imperial units"> 
+    <TwoColumnView leftTitle="Metric units" hasRightPadding={false}> 
       <div slot="left">
           <SingleUnit bind:baseValue={baseValue} bind:from={from} to="microliters" name="Microliters" />
           <SingleUnit bind:baseValue={baseValue} bind:from={from} to="milliliters" name="Milliliters" />
@@ -17,10 +19,17 @@
       </div>
   
       <div slot="right">
-        <SingleUnit bind:baseValue={baseValue} bind:from={from} to="cubic inch" name="Cubic inch" />
-        <SingleUnit bind:baseValue={baseValue} bind:from={from} to="pint" name="Pint" />
-        <SingleUnit bind:baseValue={baseValue} bind:from={from} to="gallon" name="Gallon" />
-        <SingleUnit bind:baseValue={baseValue} bind:from={from} to="cubic foot" name="Cubic feet" />
+        <StackView title="Imperial Units">
+            <SingleUnit bind:baseValue={baseValue} bind:from={from} to="cubic inch" name="Cubic inch" />
+            <SingleUnit bind:baseValue={baseValue} bind:from={from} to="pint" name="Pint" />
+            <SingleUnit bind:baseValue={baseValue} bind:from={from} to="gallon" name="Gallon" />
+            <SingleUnit bind:baseValue={baseValue} bind:from={from} to="cubic foot" name="Cubic feet" />
+            <SingleUnit bind:baseValue={baseValue} bind:from={from} to="imperial fluid ounce" name="Imperial fluid ounces" />
+        </StackView>
+
+        <StackView title="US-units">
+            <SingleUnit bind:baseValue={baseValue} bind:from={from} to="US fluid ounce" name="US fluid ounces" />
+        </StackView>
       </div>
     </TwoColumnView>
   </Tool>
