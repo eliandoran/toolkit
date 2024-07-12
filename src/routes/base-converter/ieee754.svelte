fdcfe62d82373b13892e0bafebbcdf758690f72a
Elian Doran <contact@eliandoran.me>
2024-07-12 23:16:35 +0300

    base-converter: Add IEE754 double

--- src/routes/base-converter/ieee754.svelte
+++ src/routes/base-converter/ieee754.svelte
@@ -4,12 +4,25 @@
     
     let decimalValue = 123.56;
     let floatValue;
+    let doubleValue;
+
+    function formatIeeeResult(buffer) {
+        return buffer
+            .reduce((str, byte) => str + byte
+                .toString(2)
+                .padStart(8, "0"), "");
+    }
 
     $: {
         if (decimalValue) {
-            const buffer = new Uint8Array(4);
-            ieee754.write(buffer, decimalValue, 0, false, 23, 4);
-            floatValue = buffer.reduce((str, byte) => str + byte.toString(2).padStart(8, "0"), "");
+            const floatBuffer = new Uint8Array(4);
+            const doubleBuffer = new Uint8Array(8);
+            
+            ieee754.write(floatBuffer, decimalValue, 0, false, 23, 4);
+            floatValue = formatIeeeResult(floatBuffer);
+
+            ieee754.write(doubleBuffer, decimalValue, 0, false, 52, 8);
+            doubleValue = formatIeeeResult(doubleBuffer);
         } else {
             floatValue = 0;
         }
@@ -24,10 +37,14 @@
     <input type="number" value={floatValue} readonly />
 </InputField>
 
+<InputField label="64-bits (double)">
+    <input type="number" value={doubleValue} readonly />
+</InputField>
+
 <style>
     input {
         font-family: var(--font-monospace);
         font-size: 1.25em;
-        width: 400px;
+        width: 100%;
     }
 </style>
