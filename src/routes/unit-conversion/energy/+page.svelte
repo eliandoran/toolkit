66b5d79a6880b6ae5f67b5ff5f2ba40886bea05b
Elian Doran <contact@eliandoran.me>
2024-07-12 22:50:07 +0300

    unit-conversion/energy: Add Joules (closes #56)

--- src/routes/unit-conversion/energy/+page.svelte
+++ src/routes/unit-conversion/energy/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import OneColumnView from "$lib/components/one-column-view.svelte";
 	import Tool from "$lib/components/tool.svelte";
+	import TwoColumnView from "$lib/components/two-column-view.svelte";
 	import SingleUnit from "../single-unit.svelte";
 
     let from = "watt-hour";
@@ -8,9 +9,18 @@
 </script>
 
 <Tool>
-    <OneColumnView title="Energy Conversion">
-        <SingleUnit bind:baseValue={baseValue} bind:from={from} to="watt-hour" name="Watt-hour" />
-        <SingleUnit bind:baseValue={baseValue} bind:from={from} to="kilowatt-hour" name="Killowatt-hour" />
-        <SingleUnit bind:baseValue={baseValue} bind:from={from} to="megawatt-hour" name="Megawatt-hour" />
-    </OneColumnView>
+    <TwoColumnView leftTitle="Watt-hours" rightTitle="Joules">
+        <div slot="left">
+            <SingleUnit bind:baseValue={baseValue} bind:from={from} to="watt-hour" name="Watt-hour" />
+            <SingleUnit bind:baseValue={baseValue} bind:from={from} to="kilowatt-hour" name="Killowatt-hour" />
+            <SingleUnit bind:baseValue={baseValue} bind:from={from} to="megawatt-hour" name="Megawatt-hour" />
+        </div>
+
+        <div slot="right">
+            <SingleUnit bind:baseValue={baseValue} bind:from={from} to="joules" name="Joules" />
+            <SingleUnit bind:baseValue={baseValue} bind:from={from} to="kilojoules" name="Kilojoules" />
+            <SingleUnit bind:baseValue={baseValue} bind:from={from} to="megajoules" name="Megajoules" />
+            <SingleUnit bind:baseValue={baseValue} bind:from={from} to="gigajoules" name="Gigajoules" />
+        </div>
+    </TwoColumnView>
 </Tool>
