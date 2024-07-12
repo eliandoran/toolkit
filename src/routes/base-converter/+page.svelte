ebb15bd3bd600b238530f2dff13ebf76a7db3123
Elian Doran <contact@eliandoran.me>
2024-07-12 20:59:54 +0300

    base-converter: Support Roman numerals

--- src/routes/base-converter/+page.svelte
+++ src/routes/base-converter/+page.svelte
@@ -3,18 +3,33 @@
     import Tool from "$lib/components/tool.svelte";
 	import SingleBase from "./single-base.svelte";
 
-    let decimalValue = 0;
+    import romans from "romans";
+
+    let decimalValue = 1;
 
     function standardBaseConversion(radix) {
         return {
-            from: (sourceValue) => {
-                return parseInt(sourceValue, radix)
-            },
             to: (decimalValue) => {
                 return decimalValue.toString(radix);
+            },
+            from: (sourceValue) => {
+                return parseInt(sourceValue, radix)
             }
         };
     }
+
+    const romanBase = {
+        to: (decimalValue) => {
+            if (decimalValue > 0 && decimalValue < 4000) {
+                return romans.romanize(decimalValue);
+            } else {
+                return "";
+            }
+        },
+        from: (romanStr) => {
+            return romans.deromanize(romanStr);
+        }
+    };
 </script>
 
 <Tool>
@@ -23,5 +38,6 @@
         <SingleBase fn={standardBaseConversion(8)} radixName="Octal" bind:decimalValue={decimalValue} />
         <SingleBase fn={standardBaseConversion(10)} radixName="Decimal" bind:decimalValue={decimalValue} />
         <SingleBase fn={standardBaseConversion(16)} radixName="Hexadecimal" bind:decimalValue={decimalValue} />
+        <SingleBase fn={romanBase} radixName="Roman (1 up to 3999)" bind:decimalValue={decimalValue} />
     </OneColumnView>
 </Tool>
