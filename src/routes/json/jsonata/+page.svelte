0e6bc1bd94a294e22d504bd743e907c762d3a2cd
Elian Doran <contact@eliandoran.me>
2024-07-13 14:49:36 +0300

    json/jsonata: Use original example

--- src/routes/json/jsonata/+page.svelte
+++ src/routes/json/jsonata/+page.svelte
@@ -6,8 +6,9 @@
 	import TwoColumnView from "$lib/components/two-column-view.svelte";
 	import StackView from "$lib/components/stack-view.svelte";
 
-    let jsonText;
-    let jsonataText;
+    let jsonText = JSON.stringify(JSON.parse(`{"Account":{"Account Name":"Firefly","Order":[{"OrderID":"order103","Product":[{"Product Name":"Bowler Hat","ProductID":858383,"SKU":"0406654608","Description":{"Colour":"Purple","Width":300,"Height":200,"Depth":210,"Weight":0.75},"Price":34.45,"Quantity":2},{"Product Name":"Trilby hat","ProductID":858236,"SKU":"0406634348","Description":{"Colour":"Orange","Width":300,"Height":200,"Depth":210,"Weight":0.6},"Price":21.67,"Quantity":1}]},{"OrderID":"order104","Product":[{"Product Name":"Bowler Hat","ProductID":858383,"SKU":"040657863","Description":{"Colour":"Purple","Width":300,"Height":200,"Depth":210,"Weight":0.75},"Price":34.45,"Quantity":4},{"ProductID":345664,"SKU":"0406654603","Product Name":"Cloak","Description":{"Colour":"Black","Width":30,"Height":20,"Depth":210,"Weight":2},"Price":107.99,"Quantity":1}]}]}}`), null, 4);
+    let jsonataText = "$sum(Account.Order.Product.(Price * Quantity))";
+    let resultText = "";
 </script>
 
 <Tool>
@@ -33,7 +34,7 @@
             <StackView title="Result" hasPadding={false} isCompact fill>
                 <div class="codemirror-outer-wrapper result">
                     <CodeMirror
-                        value={jsonataText}                
+                        value={resultText}                
                         lang={json()}                    
                         theme={$theme}
                         lineWrapping={true}
