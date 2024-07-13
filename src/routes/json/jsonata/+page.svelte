c63f6439ef08f35fa0056e41e4cf1f850a7547bf
Elian Doran <contact@eliandoran.me>
2024-07-13 14:18:13 +0300

    json/jsonata: Add JSONata input box

--- src/routes/json/jsonata/+page.svelte
+++ src/routes/json/jsonata/+page.svelte
@@ -4,12 +4,14 @@
     import { theme } from "$lib/stores/codemirror.js";
 	import Tool from "$lib/components/tool.svelte";
 	import TwoColumnView from "$lib/components/two-column-view.svelte";
+	import StackView from "$lib/components/stack-view.svelte";
 
     let jsonText;
+    let jsonataText;
 </script>
 
 <Tool>
-    <TwoColumnView leftTitle="JSON text" rightTitle="JSONata">
+    <TwoColumnView leftTitle="JSON text" hasPadding={false}>
         <div slot="left">
             <CodeMirror
                 bind:value={jsonText}
@@ -18,7 +20,33 @@
         </div>
 
         <div slot="right">
-
+            <StackView title="JSONata" hasPadding={false} fill>
+                <div class="codemirror-outer-wrapper jsonata">
+                    <CodeMirror
+                        value={jsonataText}                
+                        lang={json()}                    
+                        theme={$theme}
+                        lineWrapping={true} />
+                </div>
+            </StackView>
         </div>
-    </TwoColumnView>
+    </TwoColumnView>    
 </Tool>
+
+<style>
+    div[slot="right"] {
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .codemirror-outer-wrapper {
+        position: relative;
+        flex-grow: 1;
+        height: 100%;        
+    }
+
+    .codemirror-outer-wrapper.jsonata {
+        min-height: 6em;
+    }
+</style>
